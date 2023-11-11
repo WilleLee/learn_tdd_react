3f3f499abfc76429addc03cb50c1971fbfd13839
Wille Lee <1992season@gmail.com>
2023-11-12 00:03:29 +0900

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+  // render(<App />); // create a virtual DOM
+  // const linkElement = screen.getByText(/learn testing library/i); // access the virtual DOM via screen
+  // expect(linkElement).toBeInTheDocument(); // assert that the virtual DOM contains the text
+  throw new Error("test");
 });
