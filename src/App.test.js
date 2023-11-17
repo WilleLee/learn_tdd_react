78767adb047219e787af5dd8edc4bf55cec153a0
Wille Lee <1992season@gmail.com>
2023-11-17 16:45:33 +0900

    getbyrole

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,9 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
-  // render(<App />); // create a virtual DOM
-  // const linkElement = screen.getByText(/learn testing library/i); // access the virtual DOM via screen
-  // expect(linkElement).toBeInTheDocument(); // assert that the virtual DOM contains the text
-  throw new Error("test");
+  render(<App />);
+  const linkElement = screen.getByRole("link", {
+    name: /learn testing library/i,
+  });
+  expect(linkElement).toBeInTheDocument();
 });
